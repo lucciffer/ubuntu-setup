f7012f58e22cfcda1ffec95cfef152fd1b5f3fab
lucciffer <oreo.austin2000@gmail.com>
2021-10-05 15:58:48 +0530

    changes in Docker

--- setup.sh
+++ setup.sh
@@ -41,27 +41,13 @@ sh ./Miniconda3-py38_4.10.3-Linux-x86_64.sh
 echo "Miniconda installed!!"
 fi
 
-echo "Do you want to install Docker"
+echo "Do you want to install Docker?"
 read ans
 if [ "n" != $ans ]
 then 
-sudo apt-get update
-sudo apt-get install \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg \
-lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
+sh ./docker_installer.sh
 fi
+
 
 echo "Do you want to install Chrome (y/n)?"
 read ans
