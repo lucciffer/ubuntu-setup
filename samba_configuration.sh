3bfa639b06d419beeb82001111f3cd8c00503ef0
lucciffer <oreo.austin2000@gmail.com>
2021-10-13 13:12:26 +0530

    changes

--- samba_configuration.sh
+++ samba_configuration.sh
@@ -4,7 +4,7 @@ sudo apt install samba
 u="echo $USER"
 mkdir ~/sambashare
 
-sudo cat samba-conf.txt >> /etc/samba/smb.conf
+sudo cat ./samba-conf.txt >> /etc/samba/smb.conf
 sudo service smbd restart
 sudo ufw allow samba
 
